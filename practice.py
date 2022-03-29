021bdf401969e26c5024d33b991d51dec0132c85
Upcircle2 <epepep0938@gmail.com>
2022-03-29 19:15:52 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,91 @@
+'''hobby = "gaming"
+name = "상원"
+friend = "준영"
+age = 23
+adult = age >= 21
+
+print("내 이름은 " + name + " 이고 취미는 " + hobby + " 이고" 
+" 친구의 이름은 " , friend , "입니다. 나이는 ", str(age) +"입니다")
+print(name,"는 어른일까요? " ,(adult))
+#주석은 포함되지않음
+
+
+
+A1= "사당"
+A2= "신도림"
+A3= "인천공항"
+
+
+print (A1 +"행 열차가 들어오고 있습니다")
+print (A2 +"행 열차가 들어오고 있습니다")
+print (A3 +"행 열차가 들어오고 있습니다")
+
+
+print(9*9)
+print(2>=2)
+print(3+4 == 7) 
+print(1 != 3)
+print(not(2==2))
+print((1<2) | (3>5))
+
+number= 2
+print(number)
+number +=2
+print(number)
+number +=2
+print(number)
+number -=2
+print(number)
+
+number =
+number +=
+number -=
+number *=
+number /=
+
+print(1/2)
+print(2**4)
+
+from math import *
+print(floor(4.99))
+print(ceil(4.99))
+ 
+from random import *
+print(random())
+print(random() * 10)
+print(int(random() * 10) +1 )
+
+print(int(random() * 45) + 1)
+print(randrange(1,3))
+print(randint(1,3))'''
+
+
+from random import *
+date= str(randint(4,28))
+print("오프라인 스터디 모임 날짜는 매월" + date +"일로 선정되었습니다")
+
+sentence= """
+나는 컴퓨터를
+하는것을
+좋아해요"""
+
+print (sentence)
+
+
+sangwon= "010228-3042120"
+
+print(sangwon[5:])
+
+python="spiderman"
+print(python[0].isupper())#문자가 소문자인지 확인하는 열
+print(len(python))
+print(python.replace("spider","black spider"))#문자함수 재배치
+print(python)
+
+index = python.index("s")# python의 s의 위치
+print(index)
+
+
+
+
+
